4d311dec594811debcda82069bfdd72906101233
krolxon <krolyxon@tutanota.com>
2026-01-04 23:54:56 +0530

    use clap for arguement parsing

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,24 @@ mod memory;
 
 use cpu::CPU;
 use memory::Memory;
-
+use clap::Parser;
 use crate::assembler::assembler;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    filename: String,
+}
+
+
 fn main() {
+    let args = Args::parse();
+
     let mut cpu = CPU::default();
     let mut mem = Memory::new();
 
-    let asm = std::fs::read_to_string("program.asm").unwrap();
+    let asm = std::fs::read_to_string(args.filename).unwrap();
     let program = assembler(&asm);
 
     for (i, byte) in program.iter().enumerate() {
